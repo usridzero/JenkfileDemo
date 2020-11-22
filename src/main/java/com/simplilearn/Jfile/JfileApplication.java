f2c7e25db96557c9374f28adae3c9628795eb1be
student <usridzero@gmail.com>
2020-11-22 14:09:21 +0000

    First commit.

---
+++ src/main/java/com/simplilearn/Jfile/JfileApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Jfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/Jfile/JfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Jfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
